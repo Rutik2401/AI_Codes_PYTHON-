008ed3066885586f32ec23b9aa37f4a734663a22
Rutik <97967645+Rutik2401@users.noreply.github.com>
2022-05-09 22:53:11 +0530

    Update Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,24 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import sys
-A = [64, 25, 12, 22, 11]
+A = [23,64,83,47,25,68]
+
+
 for i in range(len(A)):
-    min_idx = i
-    for j in range(i+1, len(A)):
-        if A[min_idx] > A[j]:
-                min_idx = j
-    A[i], A[min_idx] = A[min_idx], A[i]
+	
+	min_idx = i
+	for j in range(i+1, len(A)):
+		if A[min_idx] > A[j]:
+			min_idx = j
+
+		
+	A[i], A[min_idx] = A[min_idx], A[i]
+
+
 print ("Sorted array")
 for i in range(len(A)):
-    print("%d" %A[i])
-
-
-# In[ ]:
-
-
-
-
+	print("%d" %A[i],end=" ")
